8c7aa8d172b005a4e2af2c0beab870f02465d62d
Tada <thomas.bernard72@outlook.com>
2018-09-01 17:56:01 +0200

    Ajoutez des fichiers projet.

---
+++ The Storyteller/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace The_Storyteller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
